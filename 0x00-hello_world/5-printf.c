cb191338d987ea75b1bd4467f8fe0ed1555b6c2d
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-02-16 14:13:27 -0800

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'using printf to print out followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
