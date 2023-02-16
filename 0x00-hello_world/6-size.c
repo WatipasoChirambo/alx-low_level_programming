543a594686e3755cdec31d01c5d8a07ece72a85f
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-02-16 14:21:29 -0800

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: printing size of values
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(a));
+}
