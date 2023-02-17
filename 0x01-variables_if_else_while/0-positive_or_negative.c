d735bf34f7ee568ca044741e6786671fe42bbd46
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-02-17 12:44:15 -0800

    If else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,25 +8,23 @@
  *
  * description: if else statements in c
  *
- * Return: (0) success
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
 	int n;
-	srand(time(0));
 
+	srand(time(0));
 	n = rand()- RAND_MAX/2;
+
 	if(n > 0)
-	{
 		printf("%d is positive\n",n);
-	}else if(n == 0)
-	{
+	else if(n == 0)
+	
 		printf("%d is zero\n", n);
-	}else
-	{
+	else
 		printf("%d is negative\n", n);
-	}
 
 	return (0);
 }
