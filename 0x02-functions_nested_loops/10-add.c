b4a99ce9eeccab40af48a713b415142d713d818f
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-02-21 12:52:09 -0800

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ *
+ * add - Add numbers a and b
+ *
+ * Description: Function adding two numbers and returing result
+ *
+ * Return: Result to add a and b
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a+b);
+}
