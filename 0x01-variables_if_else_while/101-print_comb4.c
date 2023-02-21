a060b070498e95abc4977c8bdf29d9f52fd694db
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-02-20 17:47:37 -0800

    Combinations of three numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int one, two, three;
+
+	for (one = '0'; one < '9'; one++)
+	{
+		for (two = one + 1; two <= '9'; two++)
+		{
+			for(three = p + 1; three <= '9'; three)
+			{
+				if((two != one) != three)
+				{
+					putchar(one);
+					putchar(two);
+					putchar(three);
+
+					if (one == '7' && two == '8')
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
