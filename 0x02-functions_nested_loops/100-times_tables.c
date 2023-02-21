ede6897623c8643ce642f5f438cedc2c07b5e01e
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-02-21 15:06:22 -0800

    Times table again

---
+++ 0x02-functions_nested_loops/100-times_tables.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table
+ *
+ * @n: input integer
+ *
+ * Return: Nothing
+ */
+
+void print_times_tables(int n)
+{
+	int i,j;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+				formatter(i * j);
+			_putchar('\n');
+		}
+	}
+}
+
+void formatter(int n)
+{
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
+
