18e08737ba4d86f1cc12c5b8e7cd06e141ac8dc5
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-02-21 14:01:15 -0800

    Is Alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *
+ * _isalpha - checking if its an alphabet
+ *
+ * Description: function return 1 if the character is a letter, lower or uppercase
+ *
+ * Return: 1 or 0
+ *
+ */
+
+int _isalpha(int c)
+{
+	char lower, upper;
+	int letter = 0;
+
+	for (lower = '; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+				letter = 1;
+		}
+	}
+	return (letter)
+}
