c199c6f59f0b86a8ebc10df4947e1669856643de
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-02-21 13:39:38 -0800

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@
  *
  */
 
-void times_table(void)
+void times_table(int n)
 {
 	int i,j,n;
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,4 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int add(int, int);
 void print_to_98(int n);
-void times_table(int n);
+void times_table(void);
