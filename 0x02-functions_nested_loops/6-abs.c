b6d2f64327e7a7a5ab44078fa8892855b67ae048
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-02-21 14:54:31 -0800

    Absolte number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * @r: integer input
+ *
+ * Description: function returns absolute value of number
+ *
+ * Return: absolute value of number r
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else 
+		return (r * -1);
+}
