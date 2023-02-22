7913b3ae00144b34fda2c802cf828cc7293e25ea
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-02-22 08:41:07 -0800

    Debugging infinte loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Avoiding infinite loop
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop coming :(\n");
+
+	i = 0;
+
+	/* while (i < 10)*/
+	/* {*/
+	/*	putchar(i);*/
+	/* }*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+
+}
