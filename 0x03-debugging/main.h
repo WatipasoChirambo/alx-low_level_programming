0497f0097f60dc0c42bafbab0d0b2d8268d77000
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-02-23 16:45:02 -0800

    Leap year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,7 +5,7 @@
 
 void positive_or_negative(int);
 int largest_number(int, int, int);
-void print_remainig_days(int, int, int);
-int convert_day(int, int);
+void print_remainig_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif 
