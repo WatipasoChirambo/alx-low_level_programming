37941c508cceb3558e48216712ae6dc9105bb052
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-03-09 14:00:58 -0800

    RevRecursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,4 +14,21 @@ void _puts_recursion(char *s)
 	}
 	else
 		_putchar('\n');
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ * @s: An input string to printing
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+        if (*s)
+        {
+                _putchar(*s);
+                _puts_recursion(s + 1);
+        }
+        else
+                _putchar('\n');
+}
 }
