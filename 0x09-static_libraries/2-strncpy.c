67d17711beb8c401f7de5bc7dd3956b2602cfb11
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-03-30 04:59:01 -0700

    Static Libraries Function Declarations 8

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -23,3 +23,10 @@ char *_strncpy(char *dest, char *src, int n)
 	if (n > srclen)
 		n = srclen;
 
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	return (temp);
+}
--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -27,4 +27,22 @@ int _strcmp(char *s1, char *s2)
 		s2++;
 	}
 
+	s2 = s2start;
+
+	if (s1 > s2)
+		return (15);
+
+	else if (s1 < s2)
+		return (-15);
+
+	while (*s1)
+	{
+		if (*s1 < *s2)
+			return (-15);
+		if (*s1 > *s2)
+			return (15);
+		s1++;
+		s2++;
+	}
+	return (0);
 }
