0feaf11b40ac67035e1ca839d29adb962e288e63
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-04-09 12:38:56 -0700

    print number of elemsnts in list 2

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 
 /**
- * listint_len - Function returing number of elements in the list
- * @h: pointer to listint_t structure
- * Return: number of elements
+ * listint_len - A function that returns the number of elements in the list
+ * @h: A pointer to listint_t structure
+ * Return: number of elements.
  */
-
 size_t listint_len(const listint_t *h)
 {
 	size_t nodes = 0;
