2946ec8064147d76680d913297b653fdb1f2ae9d
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-04-14 11:58:22 -0700

    4. 100

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,4 +7,5 @@ void _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 void print_binary(unsigned long int n);
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif
